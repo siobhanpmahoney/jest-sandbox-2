2b6608a2c97aea18cc1434f827a7c94a01ee5c35
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-10-11 14:29:10 -0400

    added testing packages

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {mount, configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('<App /> shallow renders', () => {
+  it('correctly shallow renders', () => {
+    const wrapper = shallow(<App />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
